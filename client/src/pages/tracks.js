fe872f69a6ca1f2e1f752bc7804c100e44f77442
milaek <56858892+milaek@users.noreply.github.com>
2021-04-18 11:57:43 -0700

    Update tracks.js with useQuery hook
    Allow for the export of const TRACKS.
    Set default loading and error messages for tracks query.
    Set return for query as a JSON string for testing purposes

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { Layout } from '../components';
+import {useQuery, gql} from '@apollo/client';
 
+
+export const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    } 
+  }
+`;
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const {loading, error, data} = useQuery(TRACKS);
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+  return <Layout grid>{JSON.stringify(data)}</Layout>;
 };
-
+ 
 export default Tracks;
